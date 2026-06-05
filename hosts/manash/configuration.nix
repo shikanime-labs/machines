e170255cbc19e81dec91b4019fe9626d54d3000b
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-06-05 21:57:50 +0200

    Update
    [ghstack-poisoned]

--- hosts/manash/configuration.nix
+++ hosts/manash/configuration.nix
@@ -154,6 +154,8 @@
           # RKE2 supervisor
           9345
         ];
+      };
+      interfaces.tailscale0 = {
         allowedUDPPorts = [
           # Canal (Flannel VXLAN) overlay
           8472
@@ -179,6 +181,7 @@
       "--cluster-cidr=10.42.0.0/16,2001:cafe:42::/56"
       "--secrets-encryption"
       "--service-cidr=10.43.0.0/16,2001:cafe:43::/112"
+      "--node-ip=100.74.220.28,fd7a:115c:a1e0::8d3a:dc1c"
       "--cni=multus"
       "--cni=canal"
     ];
@@ -296,7 +299,7 @@
           namespace = "kube-system";
         };
         spec.valuesContent = builtins.toJSON {
-          flannel.iface = "tailscale0";
+          flannel.iface = "enp1s0";
         };
       };
 
@@ -350,7 +353,11 @@
       enable = true;
       openFirewall = true;
       authKeyFile = config.sops.secrets.tailscale-authkey.path;
-      extraUpFlags = [ "--ssh" ];
+      extraUpFlags = [
+        "--ssh"
+        "--accept-routes"
+        "--advertise-routes=10.42.0.0/24,2001:cafe:42::/64"
+      ];
       useRoutingFeatures = "server";
     };
 
