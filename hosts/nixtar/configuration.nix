7cdd3bd33a2b94815acdf247fa6cb659a82b4ba8
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-06-12 22:43:51 +0200

    Fix nixtar sops keyFile typo
    spos-nix -> sops-nix in age.keyFile path.
    
    Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    ghstack-source-id: f6787cf37cf9d48508d8666533220d0190a80668
    Pull-Request: https://github.com/shikanime/shikanime/pull/853

--- hosts/nixtar/configuration.nix
+++ hosts/nixtar/configuration.nix
@@ -111,7 +111,7 @@ in
   sops = {
     age = {
       generateKey = true;
-      keyFile = "/var/lib/spos-nix/key.txt";
+      keyFile = "/var/lib/sops-nix/key.txt";
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
     };
     defaultSopsFile = ../../secrets/nixtar.enc.yaml;
