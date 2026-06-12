10a50c39f781730c48a7341d6ef6ae526af1c71d
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-06-12 22:18:43 +0200

    Update
    [ghstack-poisoned]

--- modules/flake/darwin.nix
+++ modules/flake/darwin.nix
@@ -3,24 +3,45 @@
 
 {
   flake = {
-    darwinConfigurations.telsha = inputs.nix-darwin.lib.darwinSystem {
-      pkgs = import inputs.nixpkgs {
-        system = "aarch64-darwin";
-        config.allowUnfree = true;
+    darwinConfigurations = {
+      kaltashar = inputs.nix-darwin.lib.darwinSystem {
+        pkgs = import inputs.nixpkgs {
+          system = "x86_64-darwin";
+          config.allowUnfree = true;
+        };
+        modules = [
+          ../../hosts/kaltashar/darwin-configuration.nix
+          inputs.home-manager.darwinModules.default
+          inputs.sops-nix.darwinModules.default
+          {
+            home-manager.sharedModules = [
+              inputs.catppuccin.homeModules.default
+              inputs.colemak.homeModules.default
+              inputs.devlib.homeModules.default
+              inputs.sops-nix.homeModules.default
+            ];
+          }
+        ];
       };
-      modules = [
-        ../../hosts/telsha/darwin-configuration.nix
-        inputs.home-manager.darwinModules.default
-        inputs.sops-nix.darwinModules.default
-        {
-          home-manager.sharedModules = [
-            inputs.catppuccin.homeModules.default
-            inputs.colemak.homeModules.default
-            inputs.devlib.homeModules.default
-            inputs.sops-nix.homeModules.default
-          ];
-        }
-      ];
+      telsha = inputs.nix-darwin.lib.darwinSystem {
+        pkgs = import inputs.nixpkgs {
+          system = "aarch64-darwin";
+          config.allowUnfree = true;
+        };
+        modules = [
+          ../../hosts/telsha/darwin-configuration.nix
+          inputs.home-manager.darwinModules.default
+          inputs.sops-nix.darwinModules.default
+          {
+            home-manager.sharedModules = [
+              inputs.catppuccin.homeModules.default
+              inputs.colemak.homeModules.default
+              inputs.devlib.homeModules.default
+              inputs.sops-nix.homeModules.default
+            ];
+          }
+        ];
+      };
     };
     packages.aarch64-darwin.telsha = self.darwinConfigurations.telsha.system;
   };
