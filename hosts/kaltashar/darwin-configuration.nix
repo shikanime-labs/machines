54eb4e615c0f75028d7781bb4bdea06f14f68b87
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-06-12 22:37:31 +0200

    Simplify sops age config on darwin hosts
    Replace the redundant age.generateKey + age.keyFile + age.sshKeyPaths
    block with just age.sshKeyPaths on both telsha and kaltashar.
    
    The generateKey + keyFile pattern is intended for NixOS headless servers
    without SSH host keys. On nix-darwin, the SSH host ed25519 key already
    serves as the machine identity for sops decryption.
    
    Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    ghstack-source-id: 927a3516a69cce5f195b1655afc7846a39fe7ae5
    Pull-Request: https://github.com/shikanime/shikanime/pull/851

--- hosts/kaltashar/darwin-configuration.nix
+++ hosts/kaltashar/darwin-configuration.nix
@@ -17,11 +17,7 @@
   '';
 
   sops = {
-    age = {
-      generateKey = true;
-      keyFile = "/var/lib/sops-nix/key.txt";
-      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-    };
+    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
     defaultSopsFile = ../../secrets/kaltashar.enc.yaml;
     defaultSopsFormat = "yaml";
     secrets.nix-access-token = { };
