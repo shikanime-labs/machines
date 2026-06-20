b336cbbc89ddcc9b9f677227eadc54e370c41e52
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-21 01:32:50 +0200

    Extract token management to identities
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I64dc8c94767b0c1b1053f679db3c84c96a6a6964
    ghstack-source-id: b13091b7b9498b6fd998e9ac4498dd2906912dd7
    Pull-Request: https://github.com/shikanime/shikanime/pull/966

--- hosts/nixtar/users/shika/home-configuration.nix
+++ hosts/nixtar/users/shika/home-configuration.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 
@@ -9,8 +8,6 @@ with lib;
 
 let
   toDhall = generators.toDhall { };
-  ini = pkgs.formats.ini { };
-  yaml = pkgs.formats.yaml { };
 in
 {
   imports = [
@@ -43,8 +40,6 @@ in
     };
   };
 
-  home.sessionVariables.GHSTACKRC_PATH = config.lib.file.mkOutOfStoreSymlink config.sops.templates.ghstack-config.path;
-
   programs = {
     bash.enable = true;
 
@@ -55,43 +50,15 @@ in
     age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
     defaultSopsFile = ../../../../secrets/nixtar.enc.yaml;
     defaultSopsFormat = "yaml";
-    secrets = {
-      cachix-token = { };
-      github-token = { };
-      gitlab-token = { };
-    };
+    secrets.cachix-token = { };
     templates = {
       cachix-config.content = toDhall {
         authToken = config.sops.placeholder.cachix-token;
         hostname = "https://cachix.org";
       };
-      ghstack-config = {
-        file = ini.generate "ghstackrc" {
-          ghstack = {
-            github_oauth = config.sops.placeholder.github-token;
-            github_url = "github.com";
-            github_username = "shikanime";
-          };
-        };
-        mode = "0640";
-      };
-      glab-cli-config.file = yaml.generate "config.yaml" {
-        git_protocol = "https";
-        hosts.gitlab.com = {
-          api_host = "gitlab.com";
-          api_protocol = "https";
-          token = config.sops.placeholder.gitlab-token;
-        };
-      };
     };
   };
 
-  xdg.configFile = {
-    "cachix/cachix.dhall".source =
-      config.lib.file.mkOutOfStoreSymlink config.sops.templates.cachix-config.path;
-    "glab-cli/config.yml" = {
-      force = true;
-      source = config.lib.file.mkOutOfStoreSymlink config.sops.templates.glab-cli-config.path;
-    };
-  };
+  xdg.configFile."cachix/cachix.dhall".source =
+    config.lib.file.mkOutOfStoreSymlink config.sops.templates.cachix-config.path;
 }
