b8b19831eae2ad1e69881de2bcb624eaffb44a74
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-06-23 22:54:04 +0200

    Update
    [ghstack-poisoned]

--- modules/nixos/nishir.nix
+++ modules/nixos/nishir.nix
@@ -125,6 +125,7 @@ with lib;
       RestartSec = "5s";
     };
     script = ''
+      ${getExe pkgs.tailscale} reset
       ${getExe pkgs.tailscale} serve --yes --bg --service=svc:nishir --https=6443 https+insecure://127.0.0.1:6443
       ${getExe pkgs.tailscale} serve --yes --bg --service=svc:nishir --https=9345 https+insecure://127.0.0.1:9345
       ${getExe pkgs.tailscale} serve --yes --bg --service=svc:syncthing --https=8384 https+insecure://127.0.0.1:443
