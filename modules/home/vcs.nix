a2821509ab95e021b8a0fb062142f9e7ef516454
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-02 18:34:27 +0200

    Update
    [ghstack-poisoned]

--- modules/home/vcs.nix
+++ modules/home/vcs.nix
@@ -71,7 +71,7 @@ with lib;
             "--all-remotes"
           ];
         };
-        git.private-commits = "description(glob:'secret:*')";
+        git.private-commits = "description(glob:'*[private]*')";
         templates = {
           commit_trailers = ''
             format_signed_off_by_trailer(self)
@@ -85,24 +85,6 @@ with lib;
         };
         ui.default-command = "log";
       };
-    };
-
-    sapling = {
-      enable = true;
-      extraConfig = {
-        committemplate = {
-          emptymsg = "{if(title, title, defaulttitle)}\n\nSummary: {summary}\n\nFixes: {fixes}\n\nSigned-off-by: {author}";
-          commit-message-fields = "Summary,Fixes,Signed-off-by";
-        };
-
-        hooks = {
-          "precommit.git-hooks" = "test -f .git/hooks/pre-commit && .git/hooks/pre-commit || true";
-          "preoutgoing.git-hooks" = "test -f .git/hooks/pre-push && .git/hooks/pre-push || true";
-          "update.git-hooks" = "test -f .git/hooks/post-rewrite && .git/hooks/post-rewrite || true";
-        };
-      };
-      userName = "Shikanime Deva";
-      userEmail = "contact@shikanime.studio";
     };
   };
 }
