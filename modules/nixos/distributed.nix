42bfaf8e1f3d607da792306bd7f6a5b5d613132d
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-03 10:57:02 +0200

    Fix known host build error
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I08bd74e554e0dce60d58d53bcb3c10dc6a6a6964

--- modules/nixos/distributed.nix
+++ modules/nixos/distributed.nix
@@ -1,4 +1,6 @@
-{ config, ... }:
+{ config, lib, ... }:
+
+with lib;
 
 let
   mkHostname = hostName: "${hostName}.taila659a.ts.net";
@@ -30,7 +32,24 @@ let
     mandatoryFeatures = [ ];
   };
 
-  mkBuildMachines = machines: builtins.filter (m: config.networking.hostName != m.hostName) machines;
+  mkBuildMachines =
+    machines:
+    let
+      selfHostname = mkHostname config.networking.hostName;
+    in
+    builtins.filter (m: selfHostname != m.hostName) machines;
+
+  mkSshKnownHost = { hostName, publicKey }: {
+    "${mkHostname hostName}".publicKey = publicKey;
+  };
+
+  mkSshKnownHosts =
+    knownHostsList:
+    let
+      selfHostname = mkHostname config.networking.hostName;
+      mergedHosts = mergeAttrsList knownHostsList;
+    in
+    filterAttrs (name: value: selfHostname != name) mergedHosts;
 in
 {
   nix = {
@@ -47,4 +66,27 @@ in
 
     settings.builders-use-substitutes = true;
   };
+
+  programs.ssh.knownHosts = mkSshKnownHosts [
+    (mkSshKnownHost {
+      hostName = "fushi";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILrDoPfjV+jRuGXdsc+TlgaL/+eO9pPqav6SG+tl1nPC";
+    })
+    (mkSshKnownHost {
+      hostName = "minish";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFWXSOEAx3pEzeoaqKV4gCCEVK3do+f2oJWlL++lGA/N";
+    })
+    (mkSshKnownHost {
+      hostName = "manash";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDW8I5803nYCNIKmzXBgfVDYpOJvYH0jg8ht5Djr72eL";
+    })
+    (mkSshKnownHost {
+      hostName = "ashira";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILxxDA8yBStWRl43qL15IvnKrLRW9Y2KlRAtEnxm4n3X";
+    })
+    (mkSshKnownHost {
+      hostName = "nalsha";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGAFFXlS4bbJnvo2CaPdKPHX2EFyrfF/KHfcwsVgOffE";
+    })
+  ];
 }
