1db98c89a6ca2e101ffed341c205d2e6b297f445
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-18 22:45:02 +0200

    Hotfix RPI5 pci nvme SSD not starting
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I77034703257d87e8346ae1378cf23b3b6a6a6964

--- modules/nixos/rpi5.nix
+++ modules/nixos/rpi5.nix
@@ -10,4 +10,15 @@
     enable = true;
     uboot.enable = true;
   };
+
+  # Samsung PM9B1 NVMe (PCIe x1) fails to probe on the Pi 5 root port.
+  # Two issues, confirmed live:
+  #   1. APST power-state entry can't be woken in time for the admin-queue Identify
+  #      -> nvme nvme0: I/O tag 24 QID 0 timeout, disable controller
+  #   2. PCIe ASPM L1 on the BCM2712 root (default policy) -> probe error -12
+  # Both must be disabled for a clean probe.
+  boot.kernelParams = [
+    "nvme_core.default_ps_max_latency_us=0"
+    "pcie_aspm=off"
+  ];
 }
