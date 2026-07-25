4b5855434890dccd004d26447d14641250e77103
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-25 16:10:28 +0200

    Re-enable shika home modules
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I0f553caca14b0096969bb1e4bf7bb7b76a6a6964

--- hosts/ishtar/configuration.nix
+++ hosts/ishtar/configuration.nix
@@ -20,9 +20,9 @@
 
   networking.hostName = "ishtar";
 
-  # home-manager.users.shika.imports = [
-  #   ./users/shika/home-configuration.nix
-  # ];
+  home-manager.users.shika.imports = [
+    ./users/shika/home-configuration.nix
+  ];
 
   sops = {
     age = {
