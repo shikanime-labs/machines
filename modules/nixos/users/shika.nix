9d2c9c071ca5205f0561419d2550227ff75970c1
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-26 14:36:23 +0200

    Add Zed to shika
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I4be5dd7641377e55ac9c83ee24c41aab6a6a6964

--- modules/nixos/users/shika.nix
+++ modules/nixos/users/shika.nix
@@ -16,6 +16,7 @@ in
     ../../../modules/home/starship.nix
     ../../../modules/home/vcs.nix
     ../../../modules/home/workstation.nix
+    ../../../modules/home/zed-editor.nix
   ];
 
   identities = {
