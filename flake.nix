272632a4248868d3c82ea6134080781931104337
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-26 04:08:51 +0200

    Fix colemak override warning
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I3ce0005d9b27eec43d2aa0efbe9c86406a6a6964

--- flake.nix
+++ flake.nix
@@ -20,7 +20,6 @@
         flake-parts.follows = "flake-parts";
         git-hooks.follows = "git-hooks";
         nixpkgs.follows = "nixpkgs";
-        noctalia-greeter.follows = "noctalia-greeter";
         treefmt-nix.follows = "treefmt-nix";
       };
     };
