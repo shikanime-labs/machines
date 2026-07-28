fd5cd090798bdc0af34df6fd76891a0ae9403216
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-28 23:28:32 +0200

    Fix missing cua-driver package
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I8062834ce42705584fe5a0793b6c9a6a6a6a6964

--- modules/flake/nixos.nix
+++ modules/flake/nixos.nix
@@ -2,13 +2,6 @@
 { inputs, ... }:
 
 let
-  aiModules = [
-    inputs.cua.nixosModules.cua-driver
-    inputs.hermes-agent.nixosModules.default
-    inputs.noctalia.nixosModules.default
-    inputs.noctalia-greeter.nixosModules.default
-  ];
-
   baseModules = [
     inputs.comin.nixosModules.comin
     inputs.sops-nix.nixosModules.default
@@ -21,27 +14,41 @@ let
     inputs.knix.nixosModules.default
   ];
 
-  beelinkClusterModules = [
-    inputs.nixos-hardware.nixosModules.common-cpu-intel
-    inputs.nixos-hardware.nixosModules.common-pc-ssd
-  ]
-  ++ aiModules
-  ++ baseModules
-  ++ clusterModules;
+  mkAiModules = system: [
+    inputs.cua.nixosModules.cua-driver
+    inputs.hermes-agent.nixosModules.default
+    inputs.noctalia.nixosModules.default
+    inputs.noctalia-greeter.nixosModules.default
+    { services.cua-driver.package = inputs.cua.packages.${system}.default; }
+  ];
 
-  rpi4ClusterModules = [
-    inputs.nixos-hardware.nixosModules.raspberry-pi-4
-  ]
-  ++ aiModules
-  ++ baseModules
-  ++ clusterModules;
+  mkBeelinkClusterModules =
+    system:
+    [
+      inputs.nixos-hardware.nixosModules.common-cpu-intel
+      inputs.nixos-hardware.nixosModules.common-pc-ssd
+    ]
+    ++ (mkAiModules system)
+    ++ baseModules
+    ++ clusterModules;
 
-  rpi5ClusterModules = [
-    inputs.nixos-hardware.nixosModules.raspberry-pi-5
-  ]
-  ++ aiModules
-  ++ baseModules
-  ++ clusterModules;
+  mkRpi4ClusterModules =
+    system:
+    [
+      inputs.nixos-hardware.nixosModules.raspberry-pi-4
+    ]
+    ++ (mkAiModules system)
+    ++ baseModules
+    ++ clusterModules;
+
+  mkRpi5ClusterModules =
+    system:
+    [
+      inputs.nixos-hardware.nixosModules.raspberry-pi-5
+    ]
+    ++ (mkAiModules system)
+    ++ baseModules
+    ++ clusterModules;
 
   workstationHomeModules = [
     inputs.catppuccin.homeModules.default
@@ -52,12 +59,33 @@ let
     inputs.noctalia.homeModules.default
   ];
 
-  workstationsModules =
-    aiModules
+  mkWorkstationsModules =
+    _system:
+    mkAiModules
     ++ baseModules
     ++ [
       { home-manager.sharedModules = workstationHomeModules; }
     ];
+
+  mkCatboxPackage =
+    system:
+    let
+      catbox = mkCatboxNixosConfiguration system;
+    in
+    catbox.config.system.build.containerdiskImage;
+
+  mkAshiraNixosConfiguration =
+    system:
+    inputs.nixpkgs.lib.nixosSystem {
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      modules = [
+        ../../hosts/ashira/configuration.nix
+      ]
+      ++ (mkWorkstationsModules system);
+    };
 
   mkCatboxNixosConfiguration =
     system:
@@ -69,116 +97,128 @@ let
       modules = [
         ../../hosts/catbox/configuration.nix
       ]
-      ++ workstationsModules;
+      ++ (mkWorkstationsModules system);
     };
 
-  mkCatbox =
+  mkFushiNixosConfiguration =
     system:
-    let
-      catbox = mkCatboxNixosConfiguration system;
-    in
-    catbox.config.system.build.containerdiskImage;
+    inputs.nixpkgs.lib.nixosSystem {
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      modules = [
+        ../../hosts/fushi/configuration.nix
+      ]
+      ++ (mkRpi4ClusterModules system);
+    };
+
+  mkIshtarNixosConfiguration =
+    system:
+    inputs.nixpkgs.lib.nixosSystem {
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      modules = [
+        ../../hosts/ishtar/configuration.nix
+        inputs.nixos-hardware.nixosModules.common-cpu-intel
+        inputs.nixos-hardware.nixosModules.common-pc-ssd
+        inputs.knix.nixosModules.default
+      ]
+      ++ (mkWorkstationsModules system);
+    };
+
+  mkManashNixosConfiguration =
+    _system:
+    inputs.nixpkgs.lib.nixosSystem {
+      pkgs = import inputs.nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+      modules = [
+        ../../hosts/manash/configuration.nix
+      ]
+      ++ (mkBeelinkClusterModules "x86_64-linux");
+    };
+
+  mkMinishNixosConfiguration =
+    _system:
+    inputs.nixpkgs.lib.nixosSystem {
+      pkgs = import inputs.nixpkgs {
+        system = "aarch64-linux";
+        config.allowUnfree = true;
+      };
+      modules = [
+        ../../hosts/minish/configuration.nix
+      ]
+      ++ (mkRpi4ClusterModules "aarch64-linux");
+    };
+
+  mkNalshaNixosConfiguration =
+    system:
+    inputs.nixpkgs.lib.nixosSystem {
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      modules = [
+        ../../hosts/nalsha/configuration.nix
+      ]
+      ++ (mkBeelinkClusterModules system);
+    };
+
+  mkNemishiNixosConfiguration =
+    system:
+    inputs.nixpkgs.lib.nixosSystem {
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      modules = [
+        ../../hosts/nemishi/configuration.nix
+      ]
+      ++ (mkRpi5ClusterModules system);
+    };
+
+  mkNixtarNixosConfiguration =
+    system:
+    inputs.nixpkgs.lib.nixosSystem {
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      modules = [
+        ../../hosts/nixtar/configuration.nix
+        inputs.nixos-wsl.nixosModules.default
+      ]
+      ++ (mkWorkstationsModules system);
+    };
 in
 {
   flake = {
     nixosConfigurations = {
-      ashira = inputs.nixpkgs.lib.nixosSystem {
-        pkgs = import inputs.nixpkgs {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
-        };
-        modules = [
-          ../../hosts/ashira/configuration.nix
-        ]
-        ++ beelinkClusterModules;
-      };
-      fushi = inputs.nixpkgs.lib.nixosSystem {
-        pkgs = import inputs.nixpkgs {
-          system = "aarch64-linux";
-          config.allowUnfree = true;
-        };
-        modules = [
-          ../../hosts/fushi/configuration.nix
-        ]
-        ++ rpi4ClusterModules;
-      };
-      ishtar = inputs.nixpkgs.lib.nixosSystem {
-        pkgs = import inputs.nixpkgs {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
-        };
-        modules = [
-          ../../hosts/ishtar/configuration.nix
-          inputs.nixos-hardware.nixosModules.common-cpu-intel
-          inputs.nixos-hardware.nixosModules.common-pc-ssd
-          inputs.knix.nixosModules.default
-        ]
-        ++ workstationsModules;
-      };
-      manash = inputs.nixpkgs.lib.nixosSystem {
-        pkgs = import inputs.nixpkgs {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
-        };
-        modules = [
-          ../../hosts/manash/configuration.nix
-        ]
-        ++ beelinkClusterModules;
-      };
-      minish = inputs.nixpkgs.lib.nixosSystem {
-        pkgs = import inputs.nixpkgs {
-          system = "aarch64-linux";
-          config.allowUnfree = true;
-        };
-        modules = [
-          ../../hosts/minish/configuration.nix
-        ]
-        ++ rpi4ClusterModules;
-      };
-      nalsha = inputs.nixpkgs.lib.nixosSystem {
-        pkgs = import inputs.nixpkgs {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
-        };
-        modules = [
-          ../../hosts/nalsha/configuration.nix
-        ]
-        ++ beelinkClusterModules;
-      };
-      nemishi = inputs.nixpkgs.lib.nixosSystem {
-        pkgs = import inputs.nixpkgs {
-          system = "aarch64-linux";
-          config.allowUnfree = true;
-        };
-        modules = [
-          ../../hosts/nemishi/configuration.nix
-        ]
-        ++ rpi5ClusterModules;
-      };
-      nixtar = inputs.nixpkgs.lib.nixosSystem {
-        pkgs = import inputs.nixpkgs {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
-        };
-        modules = [
-          ../../hosts/nixtar/configuration.nix
-          inputs.nixos-wsl.nixosModules.default
-        ]
-        ++ workstationsModules;
-      };
+      ashira = mkAshiraNixosConfiguration "x86_64-linux";
+      fushi = mkFushiNixosConfiguration "aarch64-linux";
+      ishtar = mkIshtarNixosConfiguration "x86_64-linux";
+      manash = mkManashNixosConfiguration "x86_64-linux";
+      minish = mkMinishNixosConfiguration "aarch64-linux";
+      nalsha = mkNalshaNixosConfiguration "x86_64-linux";
+      nemishi = mkNemishiNixosConfiguration "aarch64-linux";
+      nixtar = mkNixtarNixosConfiguration "x86_64-linux";
     };
 
     packages = {
       x86_64-linux = {
         ashira = self.nixosConfigurations.ashira.config.system.build.toplevel;
-        catbox = mkCatbox "x86_64-linux";
+        catbox = mkCatboxPackage "x86_64-linux";
         ishtar = self.nixosConfigurations.ishtar.config.system.build.toplevel;
         manash = self.nixosConfigurations.manash.config.system.build.toplevel;
         nalsha = self.nixosConfigurations.nalsha.config.system.build.toplevel;
         nixtar = self.nixosConfigurations.nixtar.config.system.build.tarballBuilder;
       };
       aarch64-linux = {
-        catbox = mkCatbox "aarch64-linux";
+        catbox = mkCatboxPackage "aarch64-linux";
         fushi = self.nixosConfigurations.fushi.config.system.build.toplevel;
         minish = self.nixosConfigurations.minish.config.system.build.toplevel;
         nemishi = self.nixosConfigurations.nemishi.config.system.build.toplevel;
