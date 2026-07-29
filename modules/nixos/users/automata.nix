9f0ae906fe2c57f5e0c4478e8ac1cfbecef1117c
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-29 10:51:26 +0200

    Fix formatting
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I2fd6095af95b22fd46f94763d3b7e5d36a6a6964

--- modules/nixos/users/automata.nix
+++ modules/nixos/users/automata.nix
@@ -1,13 +1,4 @@
 {
-  users.users.automata = {
-    initialHashedPassword = "";
-    isNormalUser = true;
-    home = "/home/automata";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuenA6cT5pkPEwdGvmvXRjVqFTv2QwpyYrB7gvMy0/X"
-    ];
-  };
-
   home-manager.users.automata = {
     imports = [
       ../../../modules/home/base.nix
@@ -16,5 +7,14 @@
       ../../../modules/home/workstation.nix
     ];
     programs.bash.enable = true;
+  };
+
+  users.users.automata = {
+    initialHashedPassword = "";
+    isNormalUser = true;
+    home = "/home/automata";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOuenA6cT5pkPEwdGvmvXRjVqFTv2QwpyYrB7gvMy0/X"
+    ];
   };
 }
