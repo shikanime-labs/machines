337c6466018dbee370481b9c5c514dd52b3edd7b
Shikanime Deva <22115108+shikanime@users.noreply.github.com>
2026-08-29 02:13:05 +0200

    Remove syncthing tailscale-serve service and Traefik syncthing ports (#1238)
    Change-Id: I8b50e08baeffd39734e43b6bab3b83146a6a6964
    
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>

--- modules/nixos/profiles/nishir.nix
+++ modules/nixos/profiles/nishir.nix
@@ -66,29 +66,6 @@ with lib;
       canal.backend = "host-gw";
     };
 
-    tailscale.serve.services.syncthing = {
-      endpoints."tcp:22000" = "tcp://127.0.0.1:22000";
-      advertised = true;
-    };
-  };
-
-  systemd.services.tailscale-serve-syncthing = {
-    description = "Expose RKE2 and Kubernetes APIs via Tailscale serve";
-    after = [
-      "tailscaled.service"
-      "tailscale-serve.service"
-    ];
-    wants = [ "tailscaled.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      Restart = "on-failure";
-      RestartSec = "5s";
-    };
-    script = ''
-      ${getExe pkgs.tailscale} serve --yes --bg --service=svc:syncthing --https=443 https+insecure://127.0.0.1:443
-    '';
   };
 
   systemd.services.cluster-policy-route = {
