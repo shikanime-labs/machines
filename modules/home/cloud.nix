eef50402216cc471218f2794557ac31186ba8119
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-09-09 12:58:39 +0200

    Add k9s plugins for log tailing and Flux kustomizations
    Wire three k9s plugins: stern log tails on pods, Flux Kustomization
    reconcile, and suspend with confirmation. Install stern; flux was
    already present.
    
    Design: modules/home/cloud.nix
    Related: https://github.com/shikanime-labs/machines/issues/1286
    
    Co-authored-by: Automata <automata@shikanime.studio>
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: Ifb58cd03d8f57add35a661db6873d0666a6a6964

--- modules/home/cloud.nix
+++ modules/home/cloud.nix
@@ -7,10 +7,74 @@
 
   catppuccin.k9s.enable = false;
 
+  home.packages = with pkgs; [
+    fluxcd
+    stern
+  ];
+
   programs = {
     k9s = {
       enable = true;
       settings.k9s.ui.skin = "transparent";
+
+      plugins = {
+        logs = {
+          shortCut = "Ctrl-L";
+          description = "Tail pod logs with stern";
+          scopes = [ "po" ];
+          command = "stern";
+          background = false;
+          args = [
+            "$NAME"
+            "-n"
+            "$NAMESPACE"
+            "--context"
+            "$CONTEXT"
+          ];
+        };
+
+        flux-reconcile = {
+          shortCut = "Ctrl-R";
+          description = "Reconcile Flux Kustomization";
+          scopes = [
+            "ks"
+            "kustomizations"
+          ];
+          command = "flux";
+          background = true;
+          args = [
+            "reconcile"
+            "kustomization"
+            "$NAME"
+            "-n"
+            "$NAMESPACE"
+            "--context"
+            "$CONTEXT"
+          ];
+        };
+
+        flux-suspend = {
+          shortCut = "Ctrl-S";
+          description = "Suspend Flux Kustomization";
+          scopes = [
+            "ks"
+            "kustomizations"
+          ];
+          command = "flux";
+          background = false;
+          confirm = true;
+          args = [
+            "suspend"
+            "kustomization"
+            "$NAME"
+            "-n"
+            "$NAMESPACE"
+            "--context"
+            "$CONTEXT"
+          ];
+        };
+
+      };
     };
 
     ssh.settings."ssh.dev.azure.com" = {
